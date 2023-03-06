558ee451b6b5a70515ba09fbc573202ea34dd4bb
Thomas S <intel44@users.noreply.github.com>
2023-03-06 02:11:40 +0100

    Update and rename ovh-postinstall-script.sh to cloud-postinstall-script.sh

--- cloud-postinstall-script.sh
+++ cloud-postinstall-script.sh
@@ -3,3 +3,6 @@ old_user=ubuntu
 new_user=intel
 usermod -l $new_user $old_user
 usermod -d /home/$new_user -m $old_user
+
+sudo usermod -md /home/NewHome -l NewName OldName
+sudo groupmod -n NewGroupName OldGroupName
