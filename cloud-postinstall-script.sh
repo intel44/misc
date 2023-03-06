314ff9cd768e1e2337a3debf652998e763b73b18
Thomas S <intel44@users.noreply.github.com>
2023-03-06 02:29:41 +0100

    Update cloud-postinstall-script.sh

--- cloud-postinstall-script.sh
+++ cloud-postinstall-script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 old_user=ubuntu
 new_user=intel
-usermod -l $new_user $old_user
-usermod -d /home/$new_user -m $old_user
-
-sudo usermod -md /home/NewHome -l NewName OldName
-sudo groupmod -n NewGroupName OldGroupName
+usermod -md /home/$new_user -l $new_user $old_user
+groupmod -n $new_user $old_user
