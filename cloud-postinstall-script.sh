2eda1659204b9ce141de21cc79f23705169c1dcb
Thomas S <intel44@users.noreply.github.com>
2023-05-10 19:59:52 +0200

    Update cloud-postinstall-script.sh

--- cloud-postinstall-script.sh
+++ cloud-postinstall-script.sh
@@ -3,3 +3,5 @@ old_user=ubuntu
 new_user=intel
 usermod -md /home/$new_user -l $new_user $old_user
 groupmod -n $new_user $old_user
+# change ssh port
+sed -i 's/#Port 22/Port 1204/g' /etc/ssh/sshd_config
