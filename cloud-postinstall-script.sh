52167e320cfc01dccf05793c411d88fa6d7f4a83
Thomas S <intel44@users.noreply.github.com>
2023-05-11 19:02:38 +0200

    Update cloud-postinstall-script.sh

--- cloud-postinstall-script.sh
+++ cloud-postinstall-script.sh
@@ -3,7 +3,9 @@ old_user=ubuntu
 new_user=intel
 usermod -md /home/$new_user -l $new_user $old_user
 groupmod -n $new_user $old_user
-# bypass sudo passwd for this user
+# remove ovh default configuration
+sudo rm -f /etc/sudoers.d/*
+# bypass sudo passwd for this new user
 echo "$new_user ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/dont-prompt-$new_user-for-sudo-password"
 # change ssh port
 sed -i 's/#Port 22/Port 1204/g' /etc/ssh/sshd_config
