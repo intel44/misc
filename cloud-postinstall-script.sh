b090e17de86edee2fd982326c2912060b0a53050
Thomas S <intel44@users.noreply.github.com>
2023-05-11 16:26:41 +0200

    Update

--- cloud-postinstall-script.sh
+++ cloud-postinstall-script.sh
@@ -3,5 +3,8 @@ old_user=ubuntu
 new_user=intel
 usermod -md /home/$new_user -l $new_user $old_user
 groupmod -n $new_user $old_user
+# bypass sudo passwd for this user
+echo "$new_user ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/dont-prompt-$new_user-for-sudo-password"
 # change ssh port
 sed -i 's/#Port 22/Port 1204/g' /etc/ssh/sshd_config
+reboot now
